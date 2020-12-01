c63708c886c07c9a1be99eb8cbaa065a0c214f6d
Plasnid <stuart@sackler.ca>
2020-12-01 00:53:51 -0500

    initial test of passing from App to route children

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an about page {{zeName}}</h1>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      zeName: String
+    }
+  }
+</script>
